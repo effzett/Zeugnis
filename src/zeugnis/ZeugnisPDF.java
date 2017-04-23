185c7208c114f38b58cd6a89c135261a93b86d91
Frank Zimmermann <fz@zenmeister.de>
2017-04-23 18:27:53 +0200

    erste Ausgabeseite formatiert

--- src/zeugnis/ZeugnisPDF.java
+++ src/zeugnis/ZeugnisPDF.java
@@ -33,6 +33,8 @@ import java.util.logging.Logger;
 public class ZeugnisPDF  {
     private static final Font NORMAL_FONT=new Font(Font.FontFamily.HELVETICA,12,Font.NORMAL);
     private static final Font BIGGER_FONT=new Font(Font.FontFamily.HELVETICA,14,Font.NORMAL);
+    private static final Font NAME_FONT=new Font(Font.FontFamily.HELVETICA,26,Font.NORMAL);
+    private static final Font SMALL_FONT=new Font(Font.FontFamily.HELVETICA,10,Font.NORMAL);
     private static final Font BIG_FONT=new Font(Font.FontFamily.HELVETICA,36,Font.BOLD);
     private static final Font NORMAL_BOLD_FONT=new Font(Font.FontFamily.HELVETICA,12,Font.BOLD);
     private static final Font NORMAL_FONT_RED=new Font(Font.FontFamily.HELVETICA,12,Font.NORMAL,BaseColor.RED);
@@ -51,49 +53,121 @@ public class ZeugnisPDF  {
         String Name      = "Max Mustermann";
         String Geboren   = "geboren am 01.01.2000 in Brelingen";
         String Tage      = "versäumte Unterrichtstage im 1. und 2. Halbjahr: 2 davon unentschuldigt: 0";
-        String Lernentwicklung;  // aus der DB ?????
+        String Lernentwicklung = "Lernentwicklung (kurz!)\nInteressen\nLernstand Deutsch\nLernstand Mathe\nVeränderungsprozesse\nKnackpunkte";
         
         PdfWriter writer = null;
         Document doc=new Document(PageSize.A4,50,30,20,30);
         writer=PdfWriter.getInstance(doc,new FileOutputStream(new File("MustermannMax.pdf")));
         doc.open();
-        
+
         // Logo
-        double moremargin=doc.getPageSize().getWidth()*0.05;
+        double moremargin=doc.getPageSize().getWidth()*0.0;
         Image img = Image.getInstance("OtherFiles/GSBrelingen.jpg");
         float scaler = ((doc.getPageSize().getWidth() - doc.leftMargin() - doc.rightMargin()- (float)moremargin) / img.getWidth()) * 100;
         img.scalePercent(scaler);
-        doc.add(img);
-      
-        // Logo
-        Paragraph paraLogo = new Paragraph("Grundschule Brelingen, Schulstraße 10, 30900 Wedemark",NORMAL_FONT);
-        paraLogo.setLeading(0, 1);
-        paraLogo.setAlignment(Element.ALIGN_CENTER);
         
+        // Seite 1
+        // Tablestruktur aufbauen...
+        PdfPTable table1 = new PdfPTable(3);
+        table1.setWidthPercentage(100);
+        
+        PdfPCell cell1Logo;
+        cell1Logo = new PdfPCell(img);
+        cell1Logo.setColspan(3);
+        cell1Logo.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell1Logo.setBorder(Rectangle.NO_BORDER);
+        
+        
+        // Adresse
+        PdfPCell cell1Adresse;
+        cell1Adresse = new PdfPCell(new Phrase("Grundschule Brelingen, Schulstraße 10, 30900 Wedemark",NORMAL_FONT));
+        cell1Adresse.setColspan(3);
+        cell1Adresse.setFixedHeight(60f);
+        cell1Adresse.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell1Adresse.setBorder(Rectangle.NO_BORDER);
+                
         //Zeugnis
-        Paragraph paraZeugnis = new Paragraph("Zeugnis",BIG_FONT);
-        paraZeugnis.setLeading(0, 2);
-        paraZeugnis.setAlignment(Element.ALIGN_CENTER);
+        PdfPCell cell1Zeugnis;
+        cell1Zeugnis = new PdfPCell(new Phrase("Zeugnis",BIG_FONT));
+        cell1Zeugnis.setColspan(3);
+        cell1Zeugnis.setFixedHeight(40f);
+        cell1Zeugnis.setVerticalAlignment(Element.ALIGN_BOTTOM);
+        cell1Zeugnis.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell1Zeugnis.setBorder(Rectangle.NO_BORDER);
+       
+        //Schuljahr
+        PdfPCell cell1Schuljahr;
+        cell1Schuljahr = new PdfPCell(new Phrase(Schuljahr,BIGGER_FONT));
+        cell1Schuljahr.setFixedHeight(30f);
+        cell1Schuljahr.setVerticalAlignment(Element.ALIGN_BOTTOM);
+        cell1Schuljahr.setHorizontalAlignment(Element.ALIGN_LEFT);
+        cell1Schuljahr.setBorder(Rectangle.NO_BORDER);
+
+        //Halbjahr
+        PdfPCell cell1Halbjahr;
+        cell1Halbjahr = new PdfPCell(new Phrase(Halbjahr,BIGGER_FONT));
+        cell1Halbjahr.setFixedHeight(30f);
+        cell1Halbjahr.setVerticalAlignment(Element.ALIGN_BOTTOM);
+        cell1Halbjahr.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell1Halbjahr.setBorder(Rectangle.NO_BORDER);
         
-        // Subtitle
-        String filling="                  ";
-        Paragraph paraSubTitle = new Paragraph(Schuljahr+filling + Halbjahr+filling+Klasse,BIGGER_FONT);
-        paraSubTitle.setLeading(0, 2);
+        //Klasse
+        PdfPCell cell1Klasse;
+        cell1Klasse = new PdfPCell(new Phrase(Klasse,BIGGER_FONT));
+        cell1Klasse.setFixedHeight(30f);
+        cell1Klasse.setVerticalAlignment(Element.ALIGN_BOTTOM);
+        cell1Klasse.setHorizontalAlignment(Element.ALIGN_RIGHT);
+        cell1Klasse.setBorder(Rectangle.NO_BORDER);
+
+        //Name
+        PdfPCell cell1Name;
+        cell1Name = new PdfPCell(new Phrase(Name,NAME_FONT));
+        cell1Name.setColspan(3);
+        cell1Name.setFixedHeight(40f);
+        cell1Name.setVerticalAlignment(Element.ALIGN_BOTTOM);
+        cell1Name.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell1Name.setBorder(Rectangle.NO_BORDER);
         
-       PdfPTable table = new PdfPTable(1);
-        table.setWidthPercentage(100);
-        PdfPCell cell = new PdfPCell();
-        cell.setMinimumHeight(50);
-        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        cell.setBorder(Rectangle.NO_BORDER);
-        cell.addElement(paraLogo);
-        cell.addElement(paraZeugnis);
-        cell.addElement(paraSubTitle);
+        //Geboren
+        PdfPCell cell1Geboren;
+        cell1Geboren = new PdfPCell(new Phrase(Geboren,NORMAL_FONT));
+        cell1Geboren.setColspan(3);
+        cell1Geboren.setFixedHeight(20f);
+        cell1Geboren.setVerticalAlignment(Element.ALIGN_BOTTOM);
+        cell1Geboren.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell1Geboren.setBorder(Rectangle.NO_BORDER);
+
+        //Tage
+        PdfPCell cell1Tage;
+        cell1Tage = new PdfPCell(new Phrase(Tage,SMALL_FONT));
+        cell1Tage.setColspan(3);
+        cell1Tage.setFixedHeight(50f);
+        cell1Tage.setVerticalAlignment(Element.ALIGN_TOP);
+        cell1Tage.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell1Tage.setBorder(Rectangle.NO_BORDER);
+
+        //Lernentwicklung
+        PdfPCell cell1Lernentwicklung;
+        cell1Lernentwicklung = new PdfPCell(new Phrase(Lernentwicklung,NORMAL_FONT));
+        cell1Lernentwicklung.setColspan(3);
+        cell1Lernentwicklung.setHorizontalAlignment(Element.ALIGN_LEFT);
+        cell1Lernentwicklung.setFixedHeight(200f);
+        //cell1Lernentwicklung.setBorder(Rectangle.NO_BORDER);
         
-        table.addCell(cell);
+        table1.addCell(cell1Logo);
+        table1.addCell(cell1Adresse);
+        table1.addCell(cell1Zeugnis);
+        table1.addCell(cell1Schuljahr);
+        table1.addCell(cell1Halbjahr);
+        table1.addCell(cell1Klasse);
+        table1.addCell(cell1Name);
+        table1.addCell(cell1Geboren);
+        table1.addCell(cell1Tage);
+        table1.addCell(cell1Lernentwicklung);
         
-        doc.add(table);
+        
+        doc.add(table1);
+      
         doc.addTitle("Zeugnis");
         doc.addAuthor("Grundschule Brelingen");
         doc.addCreationDate();
