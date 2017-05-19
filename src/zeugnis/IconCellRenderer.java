f238d8f07f5ae8e07719b837c71a626eabae6ec8
Jürgen Derigs <juergen.derigs@svlfg.de>
2017-05-19 16:56:28 +0200

    Datenbank wird jetzt automatisch erzeugt. Icons bleiben auch bei Verschieben der Spalten stabil.

--- src/zeugnis/IconCellRenderer.java
+++ src/zeugnis/IconCellRenderer.java
@@ -10,7 +10,10 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
 
 /**
  *
@@ -21,13 +24,15 @@ public class IconCellRenderer extends DefaultTableCellRenderer {
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        TableColumn tColumn  = table.getColumnModel().getColumn(column);
+        String columnName = (String)tColumn.getHeaderValue();
         
-        if (column == 5) {
+        if (columnName.equals("Löschen")) {
             setIcon(new ImageIcon(getClass().getResource("/zeugnis/pics/delete.png")));
             this.setHorizontalAlignment(JLabel.CENTER);
         }
 
-        if (column == 6) {
+        if (columnName.equals("Zeugnis drucken")) {
             setIcon(new ImageIcon(getClass().getResource("/zeugnis/pics/pdf.png")));
             this.setHorizontalAlignment(JLabel.CENTER);
         }
